48dc1ea1964c8f0aa6c582a576d07c7f0ef9f60d
felpenasr <felipenriccitrab@gmail.com>
2017-12-28 16:46:08 -0200

    pipes

--- src/app/camel-case.pipe.ts
+++ src/app/camel-case.pipe.ts
@@ -15,9 +15,9 @@ export class CamelCasePipe implements PipeTransform {
 
     return result;
   }
-
-
+  
   capitalize(value: String){
     return value.substr(0,1).toUpperCase()+value.substr(1).toLocaleLowerCase();
   }
+
 }
